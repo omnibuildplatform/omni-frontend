00a92af31bf70f50768e128d67a8084bb7d25dc1
chenHusky <377075038@qq.com>
2022-04-22 15:20:30 +0800

    删除无用代码

--- src/pages/build-job/BuildJob.vue
+++ src/pages/build-job/BuildJob.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import { reactive, ref, nextTick, watch, computed, onMounted, onBeforeUnmount } from 'vue';
-import { getBuildResult, queryProductData } from '@/api/api';
+import { reactive, ref, computed } from 'vue';
+import { queryProductData } from '@/api/api';
 import { defineComponent } from 'vue-demi';
 import ProductSelect from './product-select/ProductSelect.vue';
 import FileSelect from './file-select/FileSelect.vue';
 import { AnyObj } from '@/shared/interface/interface';
-import { ElScrollbar } from 'element-plus';
-import ProductProgress from './product-progress/ProductProgress.vue';
-import ProductButton from './product-button/ProductButton.vue';
 import ProductTransfer from './product-transfer/ProductTransfer.vue';
-import { getUserAuth } from '@/shared/utils/login';
 
 defineComponent({
   ProductSelect,
@@ -25,15 +21,6 @@ const props = defineProps({
       customPkg: [],
     }),
   },
-});
-onMounted(() => {
-  startLocalWS();
-});
-onBeforeUnmount(() => {
-  if (ws) {
-    // 关闭ws链接
-    ws.close();
-  }
 });
 const selectArr = reactive([
   {
@@ -96,7 +83,6 @@ const changeData = (e: string, type: string) => {
   });
 };
 
-const disabledBuildBtn = ref(false);
 const build = () => {
   const data: AnyObj = {
     arch: '',
@@ -109,129 +95,12 @@ const build = () => {
     data[item.id] = item.value;
   });
   return data;
-  // clearWsDataBar();
-  // startBuild(data).then((res: AnyObj) => {
-  //   window.localStorage.setItem('wsData', JSON.stringify(res));
-  //   createWSData(res);
-  // });
-};
-
-const startLocalWS = () => {
-  const ws = window.localStorage.getItem('wsData');
-  if (ws) {
-    // 启动ws
-    createWSData(JSON.parse(ws));
-  }
-};
-
-const clearLocalWS = () => {
-  window.localStorage.removeItem('wsData');
 };
 const productTransfer = ref<null | AnyObj>(null);
 const getProductTransferData = () => {
   return productTransfer?.value?.getTargetArr() || [];
 };
-const wsData = ref(['']);
 
-// 进度条
-const wsDataBar = reactive({
-  status: '',
-  percentage: 0,
-  download: '',
-});
-
-// 暂时使用假数据展示进度条
-const wsDataBarProcess = () => {
-  if (wsDataBar.percentage < 99) {
-    wsDataBar.percentage = Math.round(wsDataBar.percentage * 1000 + 10) / 1000;
-  }
-};
-
-// 进度条跑完后需请求接口判断是否成功，success获取url，failed获取失败日志
-const queryBuildResult = (data: string, id: string) => {
-  getBuildResult(data, id).then((res) => {
-    if (res.data.status === 'succeed') {
-      wsDataBar.percentage = 100;
-      wsDataBar.status = 'success';
-      wsDataBar.download = res.data.url;
-    } else if (res.data.status === 'failed') {
-      wsDataBar.percentage = 100;
-      wsDataBar.status = 'exception';
-      wsData.value[wsData.value.length - 1] += '\n';
-      wsData.value[wsData.value.length - 1] += res.data.error;
-    }
-  });
-};
-
-// 清空进度条数据
-const clearWsDataBar = () => {
-  wsDataBar.percentage = 0;
-  wsDataBar.status = '';
-  wsDataBar.download = '';
-  wsData.value = [''];
-};
-
-// 创建wbsocket长链接，监控日志
-let ws: WebSocket;
-const createWSData = (res: AnyObj) => {
-  const { token } = getUserAuth();
-  if (!token) {
-    return;
-  }
-  const { data, title } = res;
-  const host = window.location.host;
-  const http = window.location?.protocol?.replace('http', 'ws') || 'wss';
-  disabledBuildBtn.value = true;
-  const url = `${http}//${host}/ws/queryJobStatus?jobname=${data}`;
-  ws = new WebSocket(url, [token]);
-  let timer: NodeJS.Timeout;
-  ws.onclose = () => {
-    clearInterval(timer);
-    disabledBuildBtn.value = false;
-    wsData.value[wsData.value.length - 1] += '\n';
-    clearLocalWS();
-  };
-  ws.onopen = () => {
-    // 发送心跳
-    timer = setInterval(() => {
-      ws.send('heart');
-    }, 30000);
-  };
-  ws.onmessage = (evt) => {
-    const result = JSON.parse(evt.data);
-    if (result.code === 1) {
-      // 成功
-      queryBuildResult(data, title);
-    } else if (result.code === -1) {
-      // 失败
-      wsDataBar.status = 'exception';
-    } else if (result.code === 99) {
-      // 心跳保持
-      return;
-    } else {
-      // 数据连接中
-      wsDataBarProcess();
-    }
-    if (!(Math.round(wsDataBar.percentage * 100) % 50)) {
-      wsData.value.push('');
-    }
-    wsData.value[wsData.value.length - 1] += result?.data || '';
-  };
-};
-
-const scrollbarRef = ref<InstanceType<typeof ElScrollbar>>();
-
-// 日志数据变化，滚动条在底部
-watch(
-  () => wsData.value[wsData.value.length - 1],
-  () => {
-    nextTick(() => {
-      const dom = document.getElementById('log_contain') || ({} as HTMLElement);
-      const { scrollHeight } = dom;
-      scrollbarRef.value?.setScrollTop(scrollHeight);
-    });
-  }
-);
 defineExpose({
   build,
 });
@@ -263,24 +132,6 @@ defineExpose({
         <FileSelect :options="defaultPackages" width="100%" columns="3"></FileSelect>
       </div>
     </div>
-
-    <!-- <div class="m-b-24 flex flex-center">
-      <ProductButton class="build-btn" data="build" :disabled="disabledBuildBtn" @p-click="build"></ProductButton>
-      <ProductButton data="download" :disabled="!Boolean(wsDataBar.download)" :download="wsDataBar.download"></ProductButton>
-    </div>
-    <div class="common-content-block">
-      <div>
-        <div class="common-level-one-color common-level-two-fz">Building...</div>
-        <ProductProgress class="m-b-24 m-t-24" :value="wsDataBar.percentage" :status="wsDataBar.status"></ProductProgress>
-      </div>
-      <div class="common-text-content-block">
-        <el-scrollbar ref="scrollbarRef" height="600px">
-          <div id="log_contain" class="common-level-two-color common-level-two-fz log-contain">
-            <p v-for="item in wsData" :key="item" class="log-data">{{ item }}</p>
-          </div>
-        </el-scrollbar>
-      </div>
-    </div> -->
   </div>
 </template>
 <style lang="scss" scoped>
